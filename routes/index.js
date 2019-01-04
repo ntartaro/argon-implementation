b60b50def7282fd26074be17d108eecec9511a68
Nick Tartaro <ntartaro@gmail.com>
2019-01-03 19:59:28 -0500

    sidebar pointer, color changes

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,6 @@ const router = vertex.router()
 const sampleTemplates = require('../seed/templates.json')
 
 const templates = {}
-const landing = {}
-const resume = {}
 const categories = ['landing', 'resume', 'restaurant', 'fitness', 'realtor', 'lessons']
 
 router.get('/', (req, res) => {
@@ -21,28 +19,42 @@ router.get('/', (req, res) => {
 		landing: [
 			{name: 'landing 1'},
 			{name: 'landing 2'},
-			{name: 'landing 3'}
+			{name: 'landing 3'},
+		],		
+		restaurant: [
+			{name: 'restaurant 1', img: "https://images.pexels.com/photos/958373/pexels-photo-958373.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=500&w=500"},
+			{name: 'restaurant 2', img: "https://images.pexels.com/photos/958373/pexels-photo-958373.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=500&w=500"},
+			{name: 'restaurant 3', img: "https://images.pexels.com/photos/958373/pexels-photo-958373.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=500&w=500"}
 		],
 		resume: [
 			{name: 'resume 1'},
 			{name: 'resume 2'},
 			{name: 'resume 3'}
 		],
-		restaurant: [
-			{name: 'restaurant 1'},
-			{name: 'restaurant 2'},
-			{name: 'restaurant 3'}
-		]
+		lessons: [
+			{name: 'lessons 1'},
+			{name: 'lessons 2'},
+			{name: 'lessons 3'}
+		],
+		fitness: [
+			{name: 'fitness 1', img: 'https://as1.ftcdn.net/jpg/00/99/47/92/500_F_99479281_oJ0gapW7neDbLeOEhdjmP1fHr9KrDQkr.jpg'},
+			{name: 'fitness 2', img: 'https://as1.ftcdn.net/jpg/00/99/47/92/500_F_99479281_oJ0gapW7neDbLeOEhdjmP1fHr9KrDQkr.jpg'},
+			{name: 'fitness 3', img: 'https://as1.ftcdn.net/jpg/00/99/47/92/500_F_99479281_oJ0gapW7neDbLeOEhdjmP1fHr9KrDQkr.jpg'},
+			
+		],
+		realtor: [
+			{name: 'realtor 1'},
+			{name: 'realtor 2'},
+			{name: 'realtor 3'}
+		],
 
 	}
-
+	//>
 
 	templates[selected] = sampleTemplates
 	data['preloaded'] = JSON.stringify({
 		selected: selected,
-		templates: templates,
-		landing: landing,
-		resume: resume
+		templates: templates
 	})
 
 	res.render('index', data)
